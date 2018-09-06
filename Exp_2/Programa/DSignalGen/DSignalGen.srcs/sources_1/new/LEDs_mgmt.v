e75f345c352affac16d01ca59fc39b28a57e9a03
Benja <baarratia@uc.cl>
2018-09-06 14:48:17 -0300

    Cambios LED

--- Exp_2/Programa/DSignalGen/DSignalGen.srcs/sources_1/new/LEDs_mgmt.v
+++ Exp_2/Programa/DSignalGen/DSignalGen.srcs/sources_1/new/LEDs_mgmt.v
@@ -6,13 +6,13 @@ always @ ( in )
   casez(in)
   8'b00000000: out = 8'b00000000;
   8'b00000001: out = 8'b00000001;
-  8'b0000001z: out = 8'b00000010;
-  8'b000001zz: out = 8'b00000100;
-  8'b00001zzz: out = 8'b00001000;
-  8'b0001zzzz: out = 8'b00010000;
-  8'b001zzzzz: out = 8'b00100000;
-  8'b01zzzzzz: out = 8'b01000000;
-  8'b1zzzzzzz: out = 8'b10000000;
+  8'b0000001z: out = 8'b00000011;
+  8'b000001zz: out = 8'b00000111;
+  8'b00001zzz: out = 8'b00001111;
+  8'b0001zzzz: out = 8'b00011111;
+  8'b001zzzzz: out = 8'b00111111;
+  8'b01zzzzzz: out = 8'b01111111;
+  8'b1zzzzzzz: out = 8'b11111111;
   endcase
   end
 endmodule
