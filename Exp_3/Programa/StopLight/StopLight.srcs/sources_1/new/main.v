725f68da56bb4828bc2159689dd15f7d74eefed2
HenryBlairG <hblair1@uc.cl>
2018-09-20 12:34:29 -0300

    Bugs fixed

--- Exp_3/Programa/StopLight/StopLight.srcs/sources_1/new/main.v
+++ Exp_3/Programa/StopLight/StopLight.srcs/sources_1/new/main.v
@@ -21,17 +21,34 @@
 `include "debouncer.v"
 `include "btn_queue.v"
 `include "clk_mgmt.v"
-`include "Sem.v"
+//`include "Sem.v"
 
 
 
 module main(
-//    input clk,
-//    input btnC,
-//    input btnL,
-//    input btnR,
-//    input sw,
-//    output an,
-//    output JA
+    input clk,
+    input btnC,
+    input btnL,
+    input btnR,
+    input sw,
+    output an,
+    output JA
     );
+    
+    wire clock;
+    wire dsc1 = 0;
+    wire dsc2 = 0;
+    wire[3:0] queue1;
+    wire[3:0] queue2;
+    
+    clk_mgmt sl_clk(clk, clock);
+    
+    btn_queue queue(.in(btnL), 
+                    .in(btnR), 
+                    .in(clk), 
+                    .in(dsc1), 
+                    .in(dsc2), 
+                    .out(queue1), 
+                    .out(queue2));
+     
 endmodule
